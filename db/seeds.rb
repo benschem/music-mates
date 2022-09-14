eb88d4dade1cc1460568dfe3a551ca834afe0153
Tae Lee <taekyu.lee3@gmail.com>
2022-09-14 12:13:02 +1000

    index-WIP

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+puts "Cleaning database..."
+User.destroy_all # if Rails.en.development
+Concert.destroy_all
+Follow.destroy_all
+
+User.create!(
+  email: "music@mates.com",
+  password: "123456",
+  first_name: "Ben",
+  last_name: "Schem",
+  location: "Melbourne"
+)
+puts "Email: music@mates.com, Password: 123456."
+
+user1 = User.create({first_name: "Ben", last_name: "Schem", location: "Melbourne"})
+puts user1
+artist1 = Artist.create({name: "Central Cee"})
+puts artist1
+concert1 = Concert.create({date: Date.today, location: "Melbourne", description: "My bitch is gay", venue: "Forum"})
+puts concert1
+
+follows1 = Follow.create
+
+follows1.artist = artist1
+follows1.user = user1
