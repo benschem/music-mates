60a83fe0736b9d59b2f447d9ab7c9c9097038fa3
Micah <lavithian@hotmail.com>
2022-09-15 18:37:26 +0930

    Chatbox added to Group Show

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,6 +2,7 @@ class Group < ApplicationRecord
   belongs_to :concert
   has_many :invitations
   has_many :users, through: :invitations
+  has_one :chatroom
 
   validates :concert, presence: true
 end
