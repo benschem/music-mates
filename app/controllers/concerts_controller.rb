804c933b7e003b1fdd08636b97686d94ecc2f8ee
Ben Schembri <benschembri@gmail.com>
2022-09-16 18:16:34 +1000

    Bandsintown API in progress

--- app/controllers/concerts_controller.rb
+++ app/controllers/concerts_controller.rb
@@ -3,7 +3,19 @@ class ConcertsController < ApplicationController
 
   def index
     @concerts = Concert.all
-
+    # @concerts = HTTParty.get("https://rest.bandsintown.com/artists/The%20Jezabels/events?app_id=#{ENV["BANDS_IN_TOWN_KEY"]}&date=upcoming")
+    # raise
+    # current_user.artists.each do |artist|
+    #   endpoint = HTTParty.get("https://rest.bandsintown.com/artists/#{artist.name}/events?app_id=5527da5d886f6cccbb484474f7b50f5d&date=upcoming")
+    #   Concert.first_or_create(artist: artist, date: DateTime.parse(endpoint[0]["datetime"])) do |artist|
+    #     artist: artist,
+    #     date: DateTime.parse(endpoint[0]["datetime"])
+    #     location: TODO
+    #     description: Rails::Html::FullSanitizer.new.sanitize(@concerts[0]["description"])
+    #     venue: TODO
+    #   end
+    # end
+    # @concerts = current_user.concerts
   end
 
   def show
