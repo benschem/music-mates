e300cce6ad8aff1fbddf2606179c9e0737376c24
Micah <lavithian@hotmail.com>
2022-09-20 10:21:22 +0930

    Seed photos

--- app/models/concert.rb
+++ app/models/concert.rb
@@ -4,6 +4,8 @@ class Concert < ApplicationRecord
   has_many :follows, through: :artist
   has_many :users, through: :follows
 
+  default_scope { order(date: :desc) }
+
   validates :artist, presence: true
   validates :date, presence: true
   validates :venue, presence: true
