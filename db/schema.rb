b1b3c403b54e90e31c4b43cca232ed9560820ea7
Ben Schembri <benschembri@gmail.com>
2022-09-20 14:31:43 +1000

    fixed migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_20_024358) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_20_024359) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -68,6 +68,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_20_024358) do
     t.datetime "updated_at", null: false
     t.string "latitude"
     t.string "longitude"
+    t.string "city"
+    t.string "country"
     t.index ["artist_id"], name: "index_concerts_on_artist_id"
   end
 
