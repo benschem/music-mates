159d4a47b00f617f7b8fb053f5220d41063e9dd2
Micah <lavithian@hotmail.com>
2022-09-22 08:19:26 +0930

    Some style stuff

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -4,13 +4,13 @@ class ProfilesController < ApplicationController
     @top_artists = @user.artists.first(10)
   end
 
-  def search
+  def searchs
     # Search based on what the user typed in
     ##  params[:query] gives what user typed in
     # Get the user
-    user = User.search_users(params[:query])
-    # user = User.find_by("first_name ILIKE ?", "%#{params[:query]}%")
-    raise
+    # user = User.search_users(params[:query])
+    user = User.find_by("first_name ILIKE ?", "%#{params[:query]}%")
+
     # Call the show action to display the user
     # We need a fail safe
     redirect_to profile_path(user)
