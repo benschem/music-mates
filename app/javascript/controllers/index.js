90e491addec5ccde127bac964b17f4fb4fcbec45
Micah <lavithian@hotmail.com>
2022-09-21 14:18:38 +0930

    Sweet alert installed and ready to party

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -9,3 +9,6 @@ application.register("chatroom-subscription", ChatroomSubscriptionController)
 
 import HelloController from "./hello_controller"
 application.register("hello", HelloController)
+
+import SweetAlertController from "./sweet_alert_controller"
+application.register("sweet-alert", SweetAlertController)
