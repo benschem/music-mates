692184a5c7ec7b5604409f8e33150cd5e7466ea0
Ben Schembri <benschembri@gmail.com>
2022-09-21 14:53:04 +1000

    Added missing staff, added follows for Micah and Chloe

--- db/artist_repo.rb
+++ db/artist_repo.rb
@@ -1,121 +1,129 @@
 @seed_artists = [
   { name: "The Jezabels",
-  image_url: "https://i.scdn.co/image/ab6761610000e5eb71100a215b838367c49d034a",
-  spotify_link: "https://open.spotify.com/artist/76KHdORVQMt7L6nVzvOUph" },
+    image_url: "https://i.scdn.co/image/ab6761610000e5eb71100a215b838367c49d034a",
+    spotify_link: "https://open.spotify.com/artist/76KHdORVQMt7L6nVzvOUph" },
 
   { name: "The Hard Aches",
-  image_url: "https://i.scdn.co/image/ab6761610000e5eb08f1d90bfc5fac4489a01d61",
-  spotify_link: "https://open.spotify.com/artist/5GqFd0P1Vx3kbKyFxxTkqq" },
+    image_url: "https://i.scdn.co/image/ab6761610000e5eb08f1d90bfc5fac4489a01d61",
+    spotify_link: "https://open.spotify.com/artist/5GqFd0P1Vx3kbKyFxxTkqq" },
 
   { name: "Florence + The Machine",
-  image_url: "https://i.scdn.co/image/ab6761610000e5ebe3c37f869b830d1cf1ec829a",
-  spotify_link: "https://open.spotify.com/artist/1moxjboGR7GNWYIMWsRjgG" },
+    image_url: "https://i.scdn.co/image/ab6761610000e5ebe3c37f869b830d1cf1ec829a",
+    spotify_link: "https://open.spotify.com/artist/1moxjboGR7GNWYIMWsRjgG" },
 
   { name: "Stormzy",
-  image_url: "https://i.scdn.co/image/ab6761610000e5ebcd5dbc48254bfbdd35c1403b",
-  spotify_link: "https://open.spotify.com/artist/2SrSdSvpminqmStGELCSNd" },
+    image_url: "https://i.scdn.co/image/ab6761610000e5ebcd5dbc48254bfbdd35c1403b",
+    spotify_link: "https://open.spotify.com/artist/2SrSdSvpminqmStGELCSNd" },
 
   { name: "Hermitude",
-  image_url: "https://i.scdn.co/image/ab6761610000e5eba295db608250f4c08e4fb656",
-  spotify_link: "https://open.spotify.com/artist/3fmMaLC5jjf2N4EC2kTx0u" },
+    image_url: "https://i.scdn.co/image/ab6761610000e5eba295db608250f4c08e4fb656",
+    spotify_link: "https://open.spotify.com/artist/3fmMaLC5jjf2N4EC2kTx0u" },
 
   { name: "Gramatik",
-  image_url: "https://i.scdn.co/image/ab6761610000e5eb3f12e423eedf7f660062e7d4",
-  spotify_link: "https://open.spotify.com/artist/179BpmLkQCRIoU68Co80f5" },
+    image_url: "https://i.scdn.co/image/ab6761610000e5eb3f12e423eedf7f660062e7d4",
+    spotify_link: "https://open.spotify.com/artist/179BpmLkQCRIoU68Co80f5" },
 
   { name: "Slowly Slowly",
-  image_url: "https://i.scdn.co/image/ab6761610000e5eb624e9cc7895f2567f856aedb",
-  spotify_link: "https://open.spotify.com/artist/2NNeERUBxmB2UrLFfL1Q8e" },
+    image_url: "https://i.scdn.co/image/ab6761610000e5eb624e9cc7895f2567f856aedb",
+    spotify_link: "https://open.spotify.com/artist/2NNeERUBxmB2UrLFfL1Q8e" },
 
   { name: "Alison Wonderland",
-  image_url: "https://i.scdn.co/image/ab6761610000e5ebfa254d259626f0df7463e5ee",
-  spotify_link: "https://open.spotify.com/artist/11gWrKZMBsGQWmobv3oNfW" },
+    image_url: "https://i.scdn.co/image/ab6761610000e5ebfa254d259626f0df7463e5ee",
+    spotify_link: "https://open.spotify.com/artist/11gWrKZMBsGQWmobv3oNfW" },
 
   { name: "Future Static",
-  image_url: "https://i.scdn.co/image/ab6761610000e5ebcd946206019172a049de24f6",
-  spotify_link: "https://open.spotify.com/artist/00IwRuoCtg1U25AGQxDleF" },
+    image_url: "https://i.scdn.co/image/ab6761610000e5ebcd946206019172a049de24f6",
+    spotify_link: "https://open.spotify.com/artist/00IwRuoCtg1U25AGQxDleF" },
 
   { name: "Lacuna Coil",
-  image_url: "https://i.scdn.co/image/ab6761610000e5eb77ef9375d79b641976a6b989",
-  spotify_link: "https://open.spotify.com/artist/4OAddazJM576euUnFSvXSL" },
+    image_url: "https://i.scdn.co/image/ab6761610000e5eb77ef9375d79b641976a6b989",
+    spotify_link: "https://open.spotify.com/artist/4OAddazJM576euUnFSvXSL" },
 
   { name: "The Story So Far",
-  image_url: "https://i.scdn.co/image/ab6761610000e5eb93dccef6edea89fc3ddf2807",
-  spotify_link: "https://open.spotify.com/artist/6meTcQ79DrfkIuSLPZkpBg" },
+    image_url: "https://i.scdn.co/image/ab6761610000e5eb93dccef6edea89fc3ddf2807",
+    spotify_link: "https://open.spotify.com/artist/6meTcQ79DrfkIuSLPZkpBg" },
 
   { name: "Meg Mac",
-  image_url: "https://i.scdn.co/image/ab6761610000e5ebb260bb1bfb053993c1741646",
-  spotify_link: "https://open.spotify.com/artist/4faUajx9k93O56nlmpkOuz" },
+    image_url: "https://i.scdn.co/image/ab6761610000e5ebb260bb1bfb053993c1741646",
+    spotify_link: "https://open.spotify.com/artist/4faUajx9k93O56nlmpkOuz" },
 
   { name: "Ball Park Music",
-  image_url: "https://i.scdn.co/image/ab6761610000e5eb0429988bda0ebc5d59ff8076",
-  spotify_link: "https://open.spotify.com/artist/1ououJVWgWsHWMYDLvT7sH" },
+    image_url: "https://i.scdn.co/image/ab6761610000e5eb0429988bda0ebc5d59ff8076",
+    spotify_link: "https://open.spotify.com/artist/1ououJVWgWsHWMYDLvT7sH" },
 
   { name: "Thundamentals",
-  image_url: "https://i.scdn.co/image/ab6761610000e5eb6a2d110d1e923380a9773895",
-  spotify_link: "https://open.spotify.com/artist/7ymhGfvQPQLHU20RovzLMJ" },
+    image_url: "https://i.scdn.co/image/ab6761610000e5eb6a2d110d1e923380a9773895",
+    spotify_link: "https://open.spotify.com/artist/7ymhGfvQPQLHU20RovzLMJ" },
 
   { name: "Eliza & The Delusionals",
-  image_url: "https://i.scdn.co/image/ab6761610000e5ebf9480dd84f48946259485ed5",
-  spotify_link: "https://open.spotify.com/artist/5XOV5b7cxUgB9PcoLBn69l" },
+    image_url: "https://i.scdn.co/image/ab6761610000e5ebf9480dd84f48946259485ed5",
+    spotify_link: "https://open.spotify.com/artist/5XOV5b7cxUgB9PcoLBn69l" },
 
   { name: "Aitch",
-  image_url: "https://i.scdn.co/image/ab6761610000e5eb0006e67c1eed165495012076",
-  spotify_link: "https://open.spotify.com/artist/2PJEagPIxaBugeMjIyKVXF" },
+    image_url: "https://i.scdn.co/image/ab6761610000e5eb0006e67c1eed165495012076",
+    spotify_link: "https://open.spotify.com/artist/2PJEagPIxaBugeMjIyKVXF" },
 
   { name: "Horrorshow",
-  image_url: "https://i.scdn.co/image/ab6761610000e5eb8c56171ae9f62be3546a35ff",
-  spotify_link: "https://open.spotify.com/artist/4sL6dLwItg7ppqj4NwEQr6" },
+    image_url: "https://i.scdn.co/image/ab6761610000e5eb8c56171ae9f62be3546a35ff",
+    spotify_link: "https://open.spotify.com/artist/4sL6dLwItg7ppqj4NwEQr6" },
 
   { name: "Dave",
-  image_url: "https://i.scdn.co/image/ab6761610000e5eb76d47d16d18eb30413469512",
-  spotify_link: "https://open.spotify.com/artist/6Ip8FS7vWT1uKkJSweANQK" },
+    image_url: "https://i.scdn.co/image/ab6761610000e5eb76d47d16d18eb30413469512",
+    spotify_link: "https://open.spotify.com/artist/6Ip8FS7vWT1uKkJSweANQK" },
 
   { name: "Confidence Man",
-  image_url: "https://i.scdn.co/image/ab6761610000e5eb7f15bc02ab163a9f585f07fe",
-  spotify_link: "https://open.spotify.com/artist/0RwXnFrEoI8tltFvYpJgP6" },
+    image_url: "https://i.scdn.co/image/ab6761610000e5eb7f15bc02ab163a9f585f07fe",
+    spotify_link: "https://open.spotify.com/artist/0RwXnFrEoI8tltFvYpJgP6" },
 
   { name: "The Killers",
-  image_url: "https://i.scdn.co/image/ab6761610000e5eb207b21f3ed0ee96adce3166a",
-  spotify_link: "https://open.spotify.com/artist/0C0XlULifJtAgn6ZNCW2eu" },
+    image_url: "https://i.scdn.co/image/ab6761610000e5eb207b21f3ed0ee96adce3166a",
+    spotify_link: "https://open.spotify.com/artist/0C0XlULifJtAgn6ZNCW2eu" },
 
   { name: "Pierce Brothers",
-  image_url: "https://i.scdn.co/image/ab6761610000e5eb734b3a8027d24a98ea896ce4",
-  spotify_link: "https://open.spotify.com/artist/7vZrzoDeKFqcZ9JrDThOBB" },
+    image_url: "https://i.scdn.co/image/ab6761610000e5eb734b3a8027d24a98ea896ce4",
+    spotify_link: "https://open.spotify.com/artist/7vZrzoDeKFqcZ9JrDThOBB" },
 
   { name: "Kendrick Lamar",
-  image_url: "https://i.scdn.co/image/ab6761610000e5eb437b9e2a82505b3d93ff1022",
-  spotify_link: "https://open.spotify.com/artist/2YZyLoL8N0Wb9xBt1NhZWg" },
+    image_url: "https://i.scdn.co/image/ab6761610000e5eb437b9e2a82505b3d93ff1022",
+    spotify_link: "https://open.spotify.com/artist/2YZyLoL8N0Wb9xBt1NhZWg" },
 
   { name: "Lizzo",
-  image_url: "https://i.scdn.co/image/ab6761610000e5eb0d66b3670294bf801847dae2",
-  spotify_link: "https://open.spotify.com/artist/56oDRnqbIiwx4mymNEv7dS" },
+    image_url: "https://i.scdn.co/image/ab6761610000e5eb0d66b3670294bf801847dae2",
+    spotify_link: "https://open.spotify.com/artist/56oDRnqbIiwx4mymNEv7dS" },
 
   { name: "Frank Ocean",
-  image_url: "https://i.scdn.co/image/ab6761610000e5ebfbc3faec4a370d8393bee7f1",
-  spotify_link: "https://open.spotify.com/artist/2h93pZq0e7k5yf4dywlkpM" },
+    image_url: "https://i.scdn.co/image/ab6761610000e5ebfbc3faec4a370d8393bee7f1",
+    spotify_link: "https://open.spotify.com/artist/2h93pZq0e7k5yf4dywlkpM" },
 
   { name: "Doja Cat",
-  image_url: "https://i.scdn.co/image/ab6761610000e5eb727a2ac15afe659be999beba",
-  spotify_link: "https://open.spotify.com/artist/5cj0lLjcoR7YOSnhnX0Po5" },
+    image_url: "https://i.scdn.co/image/ab6761610000e5eb727a2ac15afe659be999beba",
+    spotify_link: "https://open.spotify.com/artist/5cj0lLjcoR7YOSnhnX0Po5" },
 
   { name: "Cardi B",
-  image_url: "https://i.scdn.co/image/ab6761610000e5eb8c2332e6c0ed96d144a91b3f",
-  spotify_link: "https://open.spotify.com/artist/4kYSro6naA4h99UJvo89HB" },
+    image_url: "https://i.scdn.co/image/ab6761610000e5eb8c2332e6c0ed96d144a91b3f",
+    spotify_link: "https://open.spotify.com/artist/4kYSro6naA4h99UJvo89HB" },
 
   { name: "Megan Thee Stallion",
-  image_url: "https://i.scdn.co/image/ab6761610000e5eb5bebfdee4c4cfea3473a51ab",
-  spotify_link: "https://open.spotify.com/artist/181bsRPaVXVlUKXrxwZfHK" },
+    image_url: "https://i.scdn.co/image/ab6761610000e5eb5bebfdee4c4cfea3473a51ab",
+    spotify_link: "https://open.spotify.com/artist/181bsRPaVXVlUKXrxwZfHK" },
 
   { name: "Jack Harlow",
-  image_url: "https://i.scdn.co/image/ab6761610000e5ebed3a04c76cbc92b97f59c3c0",
-  spotify_link: "https://open.spotify.com/artist/2LIk90788K0zvyj2JJVwkJ" },
+    image_url: "https://i.scdn.co/image/ab6761610000e5ebed3a04c76cbc92b97f59c3c0",
+    spotify_link: "https://open.spotify.com/artist/2LIk90788K0zvyj2JJVwkJ" },
 
   { name: "Chris Brown",
-  image_url: "https://i.scdn.co/image/ab6761610000e5ebe50aa80e0f5869f84f6874d1",
-  spotify_link: "https://open.spotify.com/artist/7bXgB6jMjp9ATFy66eO08Z" },
+    image_url: "https://i.scdn.co/image/ab6761610000e5ebe50aa80e0f5869f84f6874d1",
+    spotify_link: "https://open.spotify.com/artist/7bXgB6jMjp9ATFy66eO08Z" },
 
-  { name: "The Smith Street Band",
-  image_url: "https://i.scdn.co/image/ab6761610000e5ebbf7252960c8d3d1a83359d4e",
-  spotify_link: "https://open.spotify.com/artist/6DW1Yqi5B34jRtn9hPLvFp" }
+  {
+    name: "The Smith Street Band",
+    image_url: "https://i.scdn.co/image/ab6761610000e5ebbf7252960c8d3d1a83359d4e",
+    spotify_link: "https://open.spotify.com/artist/6DW1Yqi5B34jRtn9hPLvFp"
+  },
+
+  {
+    name: "Kehlani",
+    image_url: "https://i.scdn.co/image/ab6761610000e5ebd2657bcafcb300d90816523f",
+    spotify_link: "https://open.spotify.com/artist/0cGUm45nv7Z6M6qdXYQGTX"
+  }
 ]
