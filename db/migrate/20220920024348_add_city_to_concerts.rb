1555c6c70d881263bff7b3aa2efe66214c8c632f
Tae Lee <taekyu.lee3@gmail.com>
2022-09-22 10:57:04 +1000

    index fixed

--- db/migrate/20220920024348_add_city_to_concerts.rb
+++
@@ -1,5 +0,0 @@
-class AddCityToConcerts < ActiveRecord::Migration[7.0]
-  def change
-    add_column :concerts, :city, :string
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_21_083221) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_20_024359) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -60,7 +60,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_21_083221) do
 
   create_table "concerts", force: :cascade do |t|
     t.bigint "artist_id", null: false
-    t.datetime "date", precision: nil
+    t.date "date"
     t.string "location"
     t.text "description"
     t.string "venue"
